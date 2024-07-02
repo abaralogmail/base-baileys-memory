c75b23a9685d222b06c4d9f6f85e285e9676263e
abaralogmail <abaralo@hotmail.com>
2024-07-02 13:32:25 -0300

    Add Blocked_users

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/json')
+const JsonFileAdapter = require('@bot-whatsapp/database/json')
 const run = require('./mensajes/logica')
+const fs = require('fs').promises
 
-// Set para almacenar los usuarios bloqueados
-const blockedUsers = new Set();
+let blockedUsers = new Set();
+const BLOCKED_USERS_FILE = 'blocked_users.json';
+
+// Función para cargar usuarios bloqueados desde el archivo
+const loadBlockedUsers = async () => {
+    try {
+        const data = await fs.readFile(BLOCKED_USERS_FILE, 'utf8');
+        blockedUsers = new Set(JSON.parse(data));
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            console.log('Error loading blocked users:', error);
+        }
+    }
+}
+
+// Función para guardar usuarios bloqueados en el archivo
+const saveBlockedUsers = async () => {
+    try {
+        await fs.writeFile(BLOCKED_USERS_FILE, JSON.stringify([...blockedUsers]));
+    } catch (error) {
+        console.log('Error saving blocked users:', error);
+    }
+}
 
 // Función para manejar el bloqueo y desbloqueo de usuarios
 const handleBlockUnblock = async (ctx, flowDynamic) => {
     const userId = ctx.from;
     if (ctx.body === '.') {
         blockedUsers.add(userId);
-        await flowDynamic('Chat detenido. Envía ".." para reanudar.');
+        await saveBlockedUsers();
+        await flowDynamic('Has pausado el chat. Envía ".." para reanudar.');
         return true;
     } else if (ctx.body === '..') {
         blockedUsers.delete(userId);
-        await flowDynamic('Chat reanudado.');
+        await saveBlockedUsers();
+        await flowDynamic('Has reanudado el chat.');
         return true;
     }
     return false;
@@ -69,7 +93,9 @@ const flowPrincipal = addKeyword(EVENTS.WELCOME)
     .addAction(processMessage)
 
 const main = async () => {
-    const adapterDB = new MockAdapter()
+    await loadBlockedUsers();  // Cargar usuarios bloqueados al iniciar
+
+    const adapterDB = new JsonFileAdapter()
     const adapterFlow = createFlow([flowPrincipal])
     const adapterProvider = createProvider(BaileysProvider)
 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,8 @@ module.exports = {
       autorestart: true,
       watch: true,
       max_memory_restart: '1G',
+      cron_restart: '0 16 * * 1-5,0 9 * * 6',
+      cron_stop: '0 20 * * 1-5,0 12 * * 6'
     },
   ],
 };
